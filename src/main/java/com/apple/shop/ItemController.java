f4bc853559da5ea05a47955b9a9f515fb6ec68e3
lvdvpr <lvdvpr@daum.net>
2024-12-27 00:41:02 +0900

    Post 요청을 통한 상품 추가기능

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,22 @@ public class ItemController {
         List<Item> result = itemRepository.findAll();
         model.addAttribute("items", result);
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String getWriteForm() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addItem(@RequestParam(name="title") String title, @RequestParam(name="price", defaultValue = "0") String price) {
+        Item item = new Item();
+        item.setTitle(title);
+        String numberWithCommas = price;
+        String cleanedNumber = numberWithCommas.replace(",", "");
+        item.setPrice(Integer.valueOf(cleanedNumber));
+        itemRepository.save(item);
+        return "redirect:/list";
     }
 
 }
